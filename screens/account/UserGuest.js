8e79a25e0dabd0d349484ea03218cd1f2a39cc0d
unknown <electrojam@hotmail.com>
2021-05-20 20:23:02 -0500

    Navigate to login page

--- screens/account/UserGuest.js
+++ screens/account/UserGuest.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import { StyleSheet, Text, ScrollView, Image } from 'react-native'
 import { Button } from 'react-native-elements'
+import { useNavigation } from '@react-navigation/native'
 
 export default function UserGuest() {
+    const navigation = useNavigation()
+
     return (
         <ScrollView
             centerContent
@@ -20,7 +23,7 @@ export default function UserGuest() {
             <Button 
                 buttonStyle={styles.button}
                 title="Ver tu perfil"
-                onPress={() => console.log("Click!!!")}
+                onPress={() => navigation.navigate("login")}
             />
         </ScrollView>
     )
@@ -33,8 +36,7 @@ const styles = StyleSheet.create({
     image: {
         height: 300,
         width: "100%",
-        marginBottom: 10,
-        
+        marginBottom: 10
     },
     title: {
         fontWeight: "bold",
