f988430e2dd6cd27d3421738138dca2e19eea1f8
unknown <electrojam@hotmail.com>
2021-08-13 18:37:21 -0500

    ventana modal no mostrar

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
 import Navigation from './Navigations/Navigation'
+import { LogBox } from 'react-native'
+
+LogBox.ignoreAllLogs()
 
 export default function App() {
   return (
