9ccfea7cd73b3a01832da53222691934795e71f1
unknown <electrojam@hotmail.com>
2021-09-04 10:55:05 -0500

    botón y ventana nuevo restaurante

--- components/restaurants/AddRestaurantForm.js
+++ components/restaurants/AddRestaurantForm.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
-export default function restaurants() {
+export default function AddRestaurantForm({ toastRef, setLoading }) {
     return (
         <View>
-            <Text>Restaurants</Text>
+            <Text>AddRestaurantForm...</Text>
         </View>
     )
 }
