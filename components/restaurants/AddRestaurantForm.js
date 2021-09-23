acaf9fc0b1143c643d190af7ff780e198ce9fc6d
unknown <electrojam@hotmail.com>
2021-09-23 17:42:42 -0500

    No-image

--- components/restaurants/AddRestaurantForm.js
+++ components/restaurants/AddRestaurantForm.js
@@ -1,43 +1,161 @@
 import React, { useState } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { Button, Input } from 'react-native-elements'
+import { Alert, Dimensions, StyleSheet, Text, View, ScrollView } from 'react-native'
+import { Avatar, Button, Input, Icon, Image } from 'react-native-elements'
 import CountryPicker from 'react-native-country-picker-modal'
+import { map, size, filter } from 'lodash'
+import { loadImageFromGallery } from '../../utils/helpers'
+
+const widthScreen = Dimensions.get("window").width
 
 export default function AddRestaurantForm({ toastRef, setLoading, navigation }) {
+    const [formData, setFormData] = useState(defaultFormValues)
+    const [errorName, seterrorName] = useState(null)
+    const [errorDescription, seterrorDescription] = useState(null)
+    const [errorEmail, seterrorEmail] = useState(null)
+    const [errorAddress, seterrorAddress] = useState(null)
+    const [errorPhone, seterrorPhone] = useState(null)
+    const [imagesSelected, setImagesSelected] = useState([])
+    
     const addRestaurant = () => {
+        console.log(formData)
         console.log("Fuck yeah!")
     }
 
     return (
-        <View style={styles.viewContainer}>
-            <FormAdd/>
+        <ScrollView style={styles.viewContainer}>
+            <ImageRestauran
+                imageRestaurant={imagesSelected[0]}
+            />
+            <FormAdd
+                formData={formData}
+                setFormData={setFormData}
+                errorName={errorName}
+                errorDescription={errorDescription}
+                errorEmail={errorEmail}
+                errorAddress={errorAddress}
+                errorPhone={errorPhone}
+            />
+            <UploadImage
+                toastRef={toastRef}
+                imagesSelected={imagesSelected}
+                setImagesSelected={setImagesSelected}
+            />
             <Button
                 title="Crear Restaurante"
                 onPress={addRestaurant}
                 buttonStyle={styles.btnAddRestaurant}
             />
+        </ScrollView>
+    )
+}
+
+function ImageRestauran({ imageRestaurant }) {
+    return (
+        <View style={styles.viewPhoto}>
+            <Image
+                style={{ width: widthScreen, height: 200 }}
+                source={
+                    imageRestaurant
+                        ? { uri: imageRestaurant }
+                        : require("../../assets/no-image.png")
+                }
+            />
         </View>
     )
 }
 
-function FormAdd() {
+function UploadImage({ toastRef, imagesSelected, setImagesSelected }) {
+    const imageSelect = async() => {
+        const response = await loadImageFromGallery([4, 3])
+        if(!response.status) {
+            toastRef.current.show("No has seleccionado ninguna imagen", 3000)
+            return
+        }
+        setImagesSelected([...imagesSelected, response.image])
+    }
+    const removeImage = (image) => {
+        Alert.alert (
+            "Eliminar imagen",
+            "¿Estás seguro que quieres eliminar la imagen?",
+            [
+                {
+                    text: "No",
+                    style: "cancel"
+                },
+                {
+                    text: "Sí",
+                    onPress: () => {
+                        setImagesSelected(
+                            filter(imagesSelected, (imageUrl) => imageUrl !== image)
+                        )
+                    }
+                }
+            ],
+            { cancelable: false }
+        )
+    }
+
+    return (
+        <ScrollView
+            horizontal
+            style={styles.viewImages}
+        >
+        {
+            size(imagesSelected) < 10 && (
+                <Icon
+                    type="material-community"
+                    name="camera"
+                    color="#7a7a7a"
+                    containerStyle={styles.containerIcon}
+                    onPress={imageSelect}
+                />
+            )
+        }
+        {
+            map (imagesSelected, (imageRestaurant, index) => (
+                <Avatar
+                    key={index}
+                    style={styles.miniatureStyle}
+                    source={{ uri: imageRestaurant }}
+                    onPress={() => removeImage(imageRestaurant)}
+                />
+            ))
+        }
+        </ScrollView>
+    )
+}
+
+function FormAdd({ formData, setFormData, errorName, errorDescription, errorEmail, errorAddress, errorPhone }) {
     const [country, setCountry] = useState("CO")
     const [callingCode, setcallingCode] = useState("57")
     const [phone, setPhone] = useState("")
 
+    const onChange = (e, type) => {
+        setFormData({ ...formData, [type] : e.nativeEvent.text })
+    }
+
     return(
-        <View style={styles.viewForm}>
+        <ScrollView style={styles.viewForm}>
             <Input
                 placeholder="Nombre del restaurante..."
-            />
+                defaultValue={formData.name}
+                onChange={(e) => onChange(e, "name")}
+                errorMessage={errorName}
+                />
             <Input
                 placeholder="Dirección del restaurante..."
+                defaultValue={formData.address}
+                onChange={(e) => onChange(e, "address")}
+                errorMessage={errorAddress}
             />
             <Input
                 keyboardType="email-address"
-                placeholder="Nombre del restaurante..."
+                placeholder="Email del restaurante..."
+                defaultValue={formData.email}
+                onChange={(e) => onChange(e, "email")}
+                errorMessage={errorEmail}
             />
-            <View style={styles.phoneView}>
+            <ScrollView style={styles.phoneView}>
                 <CountryPicker
                     withFlag
                     withCallingCode
@@ -45,24 +163,45 @@ function FormAdd() {
                     withCallingCodeButton
                     containerStyle={styles.countryPicker}
                     countryCode={country}
-                    onSelect={(country) => {
-                        setCountry(country.cca2)
-                        setcallingCode(country.callingCode[0])
+                    onSelect={(country) => { 
+                        setFormData({
+                            ...formData,
+                            "country": country.cca2,
+                            "callingCode": country.callingCode[0]
+                        })
                     }}
                 />
                 <Input
                     placeholder="Whatsapp del restaurante..."
                     keyboardType="phone-pad"
                     containerStyle={styles.inputPhone}
+                    defaultValue={formData.phone}
+                    onChange={(e) => onChange(e, "phone")}
+                    errorMessage={errorPhone}
                 />
-            </View>
+            </ScrollView>
                 <Input
                     placeholder="Descripción del restaurante."
                     multiline
                     containerStyle={styles.textArea}
+                    defaultValue={formData.description}
+                    onChange={(e) => onChange(e, "description")}
+                    errorMessage={errorDescription}
                 />
-        </View>
+        </ScrollView>
     )
+}
+
+const defaultFormValues = () => {
+    return {
+        name: "",
+        description: "",
+        email: "",
+        phone: "",
+        address: "",
+        country: "CO",
+        callingCode: "57"
+    }
 }
 
 const styles = StyleSheet.create({
@@ -86,6 +225,29 @@ const styles = StyleSheet.create({
     btnAddRestaurant: {
         margin: 20,
         backgroundColor: "#442484"
+    },
+    viewImages: {
+        flexDirection: "row",
+        marginHorizontal: 20,
+        marginTop: 30
+    },
+    containerIcon: {
+        alignItems: "center",
+        justifyContent: "center",
+        marginRight: 10,
+        height: 70,
+        width: 79,
+        backgroundColor: "#e3e3e3"
+    },
+    miniatureStyle: {
+        width: 70,
+        height: 70,
+        marginRight: 10
+    },
+    viewPhoto: {
+        alignItems: "center",
+        height: 200,
+        marginBottom: 20
     }
 
 })
