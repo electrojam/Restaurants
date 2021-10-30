ca5bc703ef63ff7e8b7a6c14855a3d26682f81f3
unknown <electrojam@hotmail.com>
2021-10-30 15:05:52 -0500

    mostrando lista de restaurantes

--- screens/restaurants/Restaurants.js
+++ screens/restaurants/Restaurants.js
@@ -6,6 +6,8 @@ import firebase from 'firebase/app'
 
 import Loading from '../../components/Loading'
 import { getRestaurants } from '../../utils/actions'
+import { size } from 'lodash'
+import ListRestaurants from '../../components/restaurants/ListRestaurants'
 
 export default function Restaurants({ navigation }) {
     const [user, setUser] = useState(null)
@@ -14,7 +16,6 @@ export default function Restaurants({ navigation }) {
     const [loading, setLoading] = useState(false)
 
     const limitRestaurants = 7
-    console.log("restaurants", restaurants)
 
     useEffect(() => {
         firebase.auth().onAuthStateChanged((userInfo) => {
@@ -40,7 +41,18 @@ export default function Restaurants({ navigation }) {
     
     return (
         <View style={styles.viewBody}>
-            <Text>Restaurants</Text>
+            {
+                size(restaurants) > 0 ? (
+                    <ListRestaurants
+                        restaurants={restaurants}
+                        navigation={navigation}
+                    />
+                ) : (
+                    <View style={styles.notFoundView}>
+                        <Text style={styles.notFoundText}>No hay restaurantes registrados.</Text>
+                    </View>
+                )
+            }
             {
                 user && (
                     <Icon
@@ -69,5 +81,14 @@ const styles = StyleSheet.create({
         shadowColor: "black",
         shadowOffset: { width: 2, height: 2 },
         shadowOpacity: 0.5
+    },
+    notFoundView: {
+        flex:1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    notFoundText: {
+        fontSize: 18,
+        fontWeight: "bold"
     }
 })
