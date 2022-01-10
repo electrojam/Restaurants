c4883e0c1757cba23085d1e02262acef27dbf47e
unknown <electrojam@hotmail.com>
2022-01-09 20:30:07 -0500

    Carousel de imágenes

--- screens/restaurants/Restaurant.js
+++ screens/restaurants/Restaurant.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react'
-import { Alert, StyleSheet, Text, View } from 'react-native'
+import { Alert, Dimensions, ScrollView, StyleSheet, Text, View } from 'react-native'
 
+import CarouselImages from '../../components/CarouselImages'
 import Loading from '../../components/Loading'
 import { getDocumentById } from '../../utils/actions'
+
+const widthScreen = Dimensions.get("window").width
 
 export default function Restaurant({ navigation, route }) {
     const { id, name } = route.params
@@ -26,10 +29,19 @@ export default function Restaurant({ navigation, route }) {
     }
 
     return (
-        <View>
+        <ScrollView>
+            <CarouselImages
+                images={restaurant.images}
+                height={250}
+                width={widthScreen}
+            />
             <Text>{restaurant.description}</Text>
-        </View>
+        </ScrollView>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    viewBody: {
+        flex: 1
+    }
+})
